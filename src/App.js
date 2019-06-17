a68d2960fd923ba36c08d439c93d344c980ee6ef
lucymar <lucygibb68@gmail.com>
2019-06-17 08:27:18 -0400

    fixes cells

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Grid from './Grid';
-import Cell from './Cell';
 
 const createBoard = () => {
   let board = [];
