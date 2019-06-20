91cfc2563c20e03bcb820b9bf7cb17936b685a88
lucymar <lucygibb68@gmail.com>
2019-06-20 17:42:00 -0400

    checks for wins horizontally, diagonally, and ties

--- src/App.js
+++ src/App.js
@@ -22,9 +22,14 @@ const createSecondBoard = () => {
 };
 
 const findLowestCell = (board, col) => {
-  for (let i = 35 + col; i >= 0; i -= 7) {
-    if (board[i].player === null) return i;
+  for (let i = 5; i > 0; i -= 1) {
+    console.log('BOARD', board);
+
+    if (board[i][col].player === null) {
+      return i;
+    }
   }
+
   return -1;
 };
 
@@ -36,33 +41,78 @@ const switchPlayers = currentPlayer => {
 
 const getGameState = board => {
   //horizontal
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 5; j++) {
-      const index = i * 7 + j;
-      const currentGroup = board.slice(index, index + 4);
-      const result = checkGroupOfFour(currentGroup);
-      if (result !== false) return result;
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row][col + 1].player &&
+          board[row][col].player === board[row][col + 2].player &&
+          board[row][col].player === board[row][col + 3].player
+        ) {
+          console.log('WINNIGN PLAYER', board[row][col].player);
+          return board[row][col].player;
+        }
+      }
     }
   }
 
   //vertical
-  //for (let i = )
-
-  //check diagonal
-};
-
-const checkGroupOfFour = values => {
-  if (values.some(value => value.player === null)) {
-    return false;
-  } else if (
-    values[0].player === values[1].player &&
-    values[1].player === values[2].player &&
-    values[2].player === values[3].player
-  ) {
-    console.log('VALUES', values);
-    return values[1].player;
+  for (let row = 3; row < 6; row++) {
+    for (let col = 0; col < 7; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row - 1][col].player &&
+          board[row][col].player === board[row - 2][col].player &&
+          board[row][col].player === board[row - 3][col].player
+        ) {
+          console.log('WINNIGN PLAYER', board[row][col].player);
+          return board[row][col].player;
+        }
+      }
+    }
   }
-  return false;
+
+  // diagonal right
+  for (let row = 3; row < 6; row++) {
+    for (let col = 0; col <= 3; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row - 1][col + 1].player &&
+          board[row][col].player === board[row - 2][col + 2].player &&
+          board[row][col].player === board[row - 3][col + 3].player
+        ) {
+          console.log('WINNIGN PLAYER', board[row][col].player);
+          return board[row][col].player;
+        }
+      }
+    }
+  }
+
+  //diagonal left
+  for (let row = 3; row < 6; row++) {
+    for (let col = 3; col <= 6; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row - 1][col - 1].player &&
+          board[row][col].player === board[row - 2][col - 2].player &&
+          board[row][col].player === board[row - 3][col - 3].player
+        ) {
+          console.log('WINNIGN PLAYER', board[row][col].player);
+          return board[row][col].player;
+        }
+      }
+    }
+  }
+
+  //for tie
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (board[i][j].player === null) {
+        return null;
+      }
+    }
+    return 'This game has ended in a tie';
+  }
 };
 
 class App extends React.Component {
@@ -93,22 +143,19 @@ class App extends React.Component {
   }
 
   fillCell(player, j, i) {
-    console.log('player', player);
-    console.log('THE I:', i);
-    console.log('THE J:', j);
-
+    let coordinates = [j, i];
     return (
       <div
         className={`cell ${player.player}`}
-        // key={idx}
-        // onClick={this.handleClick.bind(null, idx)}
+        key={coordinates}
+        onClick={this.handleClick.bind(null, coordinates)}
         data-player={player.player}
       />
     );
   }
 
   handleClick(index) {
-    let col = index % 7;
+    let col = index[0];
     this.dropTile(col);
   }
   dropTile(col) {
@@ -116,20 +163,43 @@ class App extends React.Component {
     const currentPlayer = this.state.currentPlayer;
     const index = findLowestCell(board, col);
     const boardCopy = board.slice();
-    boardCopy[index].player = currentPlayer;
+    console.log('INDEX:', index, 'COL:', col);
+    boardCopy[index][col].player = currentPlayer;
 
     const gameState = getGameState(boardCopy);
+    console.log('board copy', boardCopy);
     console.log('GAMESTATE,', gameState);
-    this.setState({
-      board: boardCopy,
-      currentPlayer: switchPlayers(currentPlayer),
-    });
+    if (gameState === null) {
+      this.setState({
+        board: boardCopy,
+        currentPlayer: switchPlayers(currentPlayer),
+      });
+    } else {
+      if (gameState === 'playerTwo' || gameState === 'playerOne') {
+        this.setState({
+          board: boardCopy,
+          gameOver: true,
+          result: `${gameState} has won the game`,
+        });
+      } else {
+        this.setState({
+          board: boardCopy,
+          gameOver: true,
+          result: `${gameState}`,
+        });
+      }
+    }
   }
 
   render() {
     return (
       <div className="App">
         <h2>Connect Four</h2>
+        {this.state.result.length > 0 ? (
+          <h4>{this.state.result}!</h4>
+        ) : (
+          <h4>It is {this.state.currentPlayer}'s turn</h4>
+        )}
         <div className="board">{this.renderCells()}</div>
       </div>
     );
