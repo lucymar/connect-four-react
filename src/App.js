306bd5b203cc507c73483e1ad1b0abec09745b4b
lucymar <lucygibb68@gmail.com>
2019-06-25 00:17:13 -0400

    adds logic to play again button and message display

--- src/App.js
+++ src/App.js
@@ -1,108 +1,11 @@
 import React from 'react';
 import './App.css';
-
-const createBoard = () => {
-  let board = [];
-  for (let i = 0; i < 6; i++) {
-    let row = [];
-    for (let j = 0; j < 7; j++) {
-      row.push({ player: null });
-    }
-    board.push(row);
-  }
-  return board;
-};
-
-const findLowestCell = (board, col) => {
-  for (let i = 5; i >= 0; i -= 1) {
-    if (board[i][col].player === null) {
-      return i;
-    }
-  }
-  return -1;
-};
-
-const switchPlayers = currentPlayer => {
-  if (currentPlayer === 'playerOne') {
-    return 'playerTwo';
-  } else return 'playerOne';
-};
-
-const getGameState = board => {
-  //horizontal
-  for (let row = 0; row < 6; row++) {
-    for (let col = 0; col < 4; col++) {
-      if (board[row][col].player) {
-        if (
-          board[row][col].player === board[row][col + 1].player &&
-          board[row][col].player === board[row][col + 2].player &&
-          board[row][col].player === board[row][col + 3].player
-        ) {
-          console.log('WINNIGN PLAYER', board[row][col].player);
-          return board[row][col].player;
-        }
-      }
-    }
-  }
-
-  //vertical
-  for (let row = 3; row < 6; row++) {
-    for (let col = 0; col < 7; col++) {
-      if (board[row][col].player) {
-        if (
-          board[row][col].player === board[row - 1][col].player &&
-          board[row][col].player === board[row - 2][col].player &&
-          board[row][col].player === board[row - 3][col].player
-        ) {
-          console.log('WINNIGN PLAYER', board[row][col].player);
-          return board[row][col].player;
-        }
-      }
-    }
-  }
-
-  // diagonal right
-  for (let row = 3; row < 6; row++) {
-    for (let col = 0; col <= 3; col++) {
-      if (board[row][col].player) {
-        if (
-          board[row][col].player === board[row - 1][col + 1].player &&
-          board[row][col].player === board[row - 2][col + 2].player &&
-          board[row][col].player === board[row - 3][col + 3].player
-        ) {
-          console.log('WINNIGN PLAYER', board[row][col].player);
-          return board[row][col].player;
-        }
-      }
-    }
-  }
-
-  //diagonal left
-  for (let row = 3; row < 6; row++) {
-    for (let col = 3; col <= 6; col++) {
-      if (board[row][col].player) {
-        if (
-          board[row][col].player === board[row - 1][col - 1].player &&
-          board[row][col].player === board[row - 2][col - 2].player &&
-          board[row][col].player === board[row - 3][col - 3].player
-        ) {
-          console.log('WINNIGN PLAYER', board[row][col].player);
-          return board[row][col].player;
-        }
-      }
-    }
-  }
-
-  //for tie
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 7; j++) {
-      if (board[i][j].player === null) {
-        return null;
-      }
-    }
-    return 'This game has ended in a tie';
-  }
-};
+import {
+  createBoard,
+  findLowestCell,
+  switchPlayers,
+  getGameState,
+} from './utils';
 
 class App extends React.Component {
   constructor(props) {
@@ -115,21 +18,20 @@ class App extends React.Component {
     };
   }
 
-  renderCells = () => {
+  renderBoard = () => {
     const board = this.state.board;
-    console.log('BOARD', board);
     return board.map((row, key) => {
       return row.map((player, idx) => {
-        return this.fillCell(player, idx, key);
+        return this.fillCells(player, idx, key);
       });
     });
   };
 
-  fillCell = (player, j, i) => {
+  fillCells = (player, j, i) => {
     let coordinates = [j, i];
     return (
       <div
-        className={`cell ${player.player}`}
+        className={`cell`}
         key={coordinates}
         onClick={() => this.handleClick(coordinates)}
         data-player={player.player}
@@ -139,9 +41,10 @@ class App extends React.Component {
 
   handleClick = indices => {
     let col = indices[0];
-    this.dropTile(col);
+    return this.state.gameOver === false ? this.play(col) : null;
   };
-  dropTile = col => {
+
+  play = col => {
     const board = this.state.board;
     const currentPlayer = this.state.currentPlayer;
     const index = findLowestCell(board, col);
@@ -151,11 +54,11 @@ class App extends React.Component {
       });
       return;
     }
+
     const boardCopy = board.slice();
     boardCopy[index][col].player = currentPlayer;
 
     const gameState = getGameState(boardCopy);
-    console.log('GAMESTATE,', gameState);
     if (gameState === null) {
       this.setState({
         board: boardCopy,
@@ -178,6 +81,7 @@ class App extends React.Component {
       }
     }
   };
+
   handleSubmit = () => {
     this.setState({
       board: createBoard(),
@@ -188,17 +92,16 @@ class App extends React.Component {
     return (
       <div className="App">
         <h2>Connect Four</h2>
-        {this.state.result.length > 0 ? (
-          <div>
-            <h4>{this.state.result}!</h4>
-            <button onClick={() => this.handleSubmit()}>Play Again</button>
-          </div>
-        ) : (
+        <div>
+          {this.state.result.length > 0 ? <h4>{this.state.result}!</h4> : null}
+          <button onClick={() => this.handleSubmit()}>Start Over</button>
+        </div>
+        {this.state.gameOver === false ? (
           <h4 className="message" data-player={this.state.currentPlayer}>
             It is {this.state.currentPlayer}'s turn
           </h4>
-        )}
-        <div className="board">{this.renderCells()}</div>
+        ) : null}
+        <div className="board">{this.renderBoard()}</div>
       </div>
     );
   }
---
+++ src/utils.js
@@ -0,0 +1,98 @@
+export const createBoard = () => {
+  let board = [];
+  for (let i = 0; i < 6; i++) {
+    let row = [];
+    for (let j = 0; j < 7; j++) {
+      row.push({ player: null });
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+export const findLowestCell = (board, col) => {
+  for (let i = 5; i >= 0; i -= 1) {
+    if (board[i][col].player === null) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+export const switchPlayers = currentPlayer => {
+  if (currentPlayer === 'playerOne') {
+    return 'playerTwo';
+  } else return 'playerOne';
+};
+
+export const getGameState = board => {
+  //horizontal
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row][col + 1].player &&
+          board[row][col].player === board[row][col + 2].player &&
+          board[row][col].player === board[row][col + 3].player
+        ) {
+          return board[row][col].player;
+        }
+      }
+    }
+  }
+
+  //vertical
+  for (let row = 3; row < 6; row++) {
+    for (let col = 0; col < 7; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row - 1][col].player &&
+          board[row][col].player === board[row - 2][col].player &&
+          board[row][col].player === board[row - 3][col].player
+        ) {
+          return board[row][col].player;
+        }
+      }
+    }
+  }
+
+  // diagonal right
+  for (let row = 3; row < 6; row++) {
+    for (let col = 0; col <= 3; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row - 1][col + 1].player &&
+          board[row][col].player === board[row - 2][col + 2].player &&
+          board[row][col].player === board[row - 3][col + 3].player
+        ) {
+          return board[row][col].player;
+        }
+      }
+    }
+  }
+
+  //diagonal left
+  for (let row = 3; row < 6; row++) {
+    for (let col = 3; col <= 6; col++) {
+      if (board[row][col].player) {
+        if (
+          board[row][col].player === board[row - 1][col - 1].player &&
+          board[row][col].player === board[row - 2][col - 2].player &&
+          board[row][col].player === board[row - 3][col - 3].player
+        ) {
+          return board[row][col].player;
+        }
+      }
+    }
+  }
+
+  //for tie
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (board[i][j].player === null) {
+        return null;
+      }
+    }
+    return 'This game has ended in a tie';
+  }
+};
